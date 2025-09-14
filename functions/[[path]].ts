df964adafbbf7f7a45e37ef127d983fca535822c
Dr. Ernesto Lee <socrates73@gmail.com>
2025-09-13 23:16:32 -0400

    Delete functions directory

--- functions/[[path]].ts
+++
@@ -1,9 +0,0 @@
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-
-// @ts-ignore
-import * as build from "../build/server";
-
-export const onRequest = createPagesFunctionHandler({
-  build,
-  getLoadContext: (context) => context.env,
-});
