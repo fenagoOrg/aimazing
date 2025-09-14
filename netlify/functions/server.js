5ca157680111e23d7ed295313177408698a49241
Dr. Ernesto Lee <socrates73@gmail.com>
2025-09-13 23:13:11 -0400

    Delete netlify/functions directory

--- netlify/functions/server.js
+++
@@ -1,7 +0,0 @@
-const { createRequestHandler } = require("@remix-run/netlify");
-const build = require("../../build/server");
-
-exports.handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
